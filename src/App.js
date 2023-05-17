d157c19584fb6732dfc9314be6f29c7ef0f6699d
HamidYaraliOfficial <129217479+HamidYaraliOfficial@users.noreply.github.com>
2023-05-17 14:53:35 +0330

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+// import react from "react";
+import './App.scss';
+const Eye = (props) => {
+    return (
+        <div className="eye">
+            <div className="eye__ball"></div>
+        </div>
+    );
+};
+
+
+export default Eye;
